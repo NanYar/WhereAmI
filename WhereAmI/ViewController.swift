d43ad72ef472d9f49b83b0d1ed9bfd1d76d23c92
NanYar <nanyar@macbookpro113.fritz.box>
2014-11-20 23:55:04 +0100

    Code finished according to tutorial

--- WhereAmI/ViewController.swift
+++ WhereAmI/ViewController.swift
@@ -7,19 +7,90 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate
+{
+    // @IBOutlets
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!    
+    @IBOutlet weak var courseLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var altitudeLabel: UILabel!
+    @IBOutlet weak var nearestAddressLabel: UILabel!
+    
+    var locationManager: CLLocationManager! // = implicitly unwrapped optional (actual value = nil)
+    
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager = CLLocationManager()
+        locationManager.delegate = self // = this ViewController (self) is managing the locationManager (.delegate)
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // = GPS Accuracy
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
+    // CLLocationManagerDelegate
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!)
+    // invoked when new locations are available
+    // locations is an array of CLLocation objects in chronological order
+    {
+        let userLocation: CLLocation = locations[0] as CLLocation
+        latitudeLabel.text = "\(userLocation.coordinate.latitude)"
+        longitudeLabel.text = "\(userLocation.coordinate.longitude)"
+        courseLabel.text = "\(userLocation.course)"
+        speedLabel.text = "\(userLocation.speed)"
+        altitudeLabel.text = "\(userLocation.altitude)"
+        
+        CLGeocoder().reverseGeocodeLocation(userLocation, completionHandler:
+        { (placemarks, error) in
+            if error == nil
+            {
+                let myPlacemark: CLPlacemark = CLPlacemark(placemark: placemarks?[0] as CLPlacemark)
+                
+                // nil - Werte ausblenden
+                var subThoroughfare: String
+                if myPlacemark.subThoroughfare != nil
+                {
+                    subThoroughfare = myPlacemark.subThoroughfare
+                }
+                else
+                {
+                    subThoroughfare = ""
+                }
+                
+                var thoroughfare: String
+                if myPlacemark.thoroughfare != nil
+                {
+                    thoroughfare = myPlacemark.thoroughfare
+                }
+                else
+                {
+                    thoroughfare = ""
+                }
+                
+                self.nearestAddressLabel.text =
+                    "\"\(myPlacemark.name)\"\n\n" +
+                    "\(thoroughfare) \(subThoroughfare)\n" +
+                    "\(myPlacemark.ISOcountryCode)-\(myPlacemark.postalCode) \(myPlacemark.locality)\n" +
+                    "\(myPlacemark.administrativeArea)\n" +
+                    "\(myPlacemark.country)"
+            }
+            else
+            {
+                self.nearestAddressLabel.text = "Error: \(error)"
+            }
+        } )
+        //println(userLocation)
+    }
 }
-
